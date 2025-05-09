81564d42ef7a1fabe1b0b710e496acc82a718e0e
adhamqabban37 <adhamqabban37@gmail.com>
2025-05-08 20:56:21 -0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,67 @@
+body {
+  font-family: 'Arial', sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #1d1d1d;
+  color: #fff;
+}
+
+header {
+  background-color: #ffb81c; /* Yellow */
+  text-align: center;
+  padding: 50px;
+}
+
+h1 {
+  margin: 0;
+  font-size: 3em;
+  color: #ffffff;
+}
+
+p {
+  font-size: 1.2em;
+}
+
+section {
+  padding: 50px;
+  margin: 0 15%;
+  background-color: #2c2c2c;
+  border-radius: 10px;
+  margin-top: 20px;
+}
+
+.bio, .projects {
+  margin-bottom: 40px;
+}
+
+h2 {
+  color: #ffb81c; /* Yellow */
+  font-size: 2em;
+}
+
+.project {
+  background-color: #3a3a3a;
+  margin: 20px 0;
+  padding: 20px;
+  border-radius: 8px;
+}
+
+.project h3 {
+  color: #ffb81c; /* Yellow */
+  font-size: 1.6em;
+}
+
+footer {
+  text-align: center;
+  padding: 30px;
+  background-color: #000000;
+}
+
+a {
+  color: #ffb81c;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
